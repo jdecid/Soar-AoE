4cbcc4559b1fb5c9f6a9d6d6ba88b453372727aa
Josep de Cid <josep.de.cid@gmail.com>
2020-05-22 23:29:13 +0200

    Fix null pointer on output

--- src/edu/upc/fib/masd/jav/Environment.java
+++ src/edu/upc/fib/masd/jav/Environment.java
@@ -1,173 +1,159 @@
 package edu.upc.fib.masd.jav;
+
+import edu.upc.fib.masd.jav.utils.Field;
+import edu.upc.fib.masd.jav.utils.FieldState;
+import sml.Identifier;
+import sml.Kernel;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.atomic.AtomicBoolean;
 
-import edu.upc.fib.masd.jav.SoarAgent;
-import edu.upc.fib.masd.jav.CollectorAgent;
-import edu.upc.fib.masd.jav.utils.Field;
-import edu.upc.fib.masd.jav.utils.FieldState;
-import sml.Agent;
-import sml.Identifier;
-import sml.Kernel;
-import sml.smlUpdateEventId;
-import sml.Kernel.UpdateEventInterface;
+public class Environment {
+    // We keep references to Agents.
+    private ArrayList<GeneralAgent> agents;
+    // Create executor services to run Soar in since it blocks.
+    private ArrayList<ExecutorService> executors;
+    // To read user input
+    private BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
-public class Environment
-{
-	// We keep references to Agents.
-	private ArrayList<GeneralAgent> agents;
-	// Create executor services to run Soar in since it blocks.
-	private ArrayList<ExecutorService> executors;
-	// To read user input
-	private BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    public Environment(ArrayList<GeneralAgent> agents) {
+        this.agents = agents;
+        this.executors = initExecutors();
 
-	public Environment(ArrayList<GeneralAgent> agents)
-	{
-		this.agents = agents;
-		this.executors = initExecutors();
-		
-		runSystemCycle();
-	}
+        runSystemCycle();
+    }
 
-	private void runSystemCycle() {
-		try {
-			// Necessary delay (ms)
-			delay(1000);
+    private void runSystemCycle() {
+        try {
+            // Necessary delay (ms)
+            delay(1000);
 
-			// Loop
-			while (!Thread.interrupted()) {
-				System.out.println("========================");
-				System.out.println("Press enter to continue (or X to exit):");
-				String line = input.readLine();
-				
-				if (line.equalsIgnoreCase("x")) {
-					shutdown();
-				}
-				
-				runAllAgentsOneStep();
-				updateEnvironmentState();
+            // Loop
+            while (!Thread.interrupted()) {
+                System.out.println("========================");
+                System.out.println("Press enter to continue (or X to exit):");
+                String line = input.readLine();
 
-				// Necessary delay (ms)
-				delay(1000);
-			}
-		} 
-		catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		finally {
-			shutdown();
-		}
-	}
-	
-	private void runAllAgentsOneStep() {
-		for(int i=0; i<agents.size(); ++i) {
-			agents.get(i).runStep();
-		}	
-	}
-	
-	private void updateEnvironmentState() {
-		for(int i=0; i<this.agents.size(); ++i) {
-			// Update agents
-			this.agents.get(i).decreaseSatiety();
-		}	
-	}
-	
-	
-	private ArrayList<ExecutorService> initExecutors() {
-		ArrayList<ExecutorService> exec = new ArrayList<ExecutorService>();
-		for(int i=0; i<this.agents.size(); ++i) {
-			exec.add(Executors.newSingleThreadExecutor());
-		}
-		return exec;
-	}
-	
-	private void shutdown() {
-		// Shutdown the Soar interface and the executor service.
-		for(int i=0; i<this.agents.size(); ++i) {
-			this.agents.get(i).shutdown();
-			this.executors.get(i).shutdown();
-		}
-		System.exit(0);
-	}
-	
-	private void delay(int ms) {
-		try {
-			Thread.sleep(ms);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private static ArrayList<GeneralAgent> createAgents(Kernel kernel, int numBarons, int numCollectors, int numBuilders) {
-		ArrayList<GeneralAgent> allAgents = new ArrayList<GeneralAgent>();
-		
-		int food = 5;
-		int foodSatiety = 15;
-		int wood = 10;
-		int numFields = 2;
-		FieldState fieldState = FieldState.DRY;
-		int fieldYield = 2;
-		
-		// Barons
-		for(int i=0; i<numBarons; ++i) {
-			BaronAgent baron = new BaronAgent(kernel, "Baron_" + i, "SOAR_Codes/PRESET_baron_agent.soar", food, foodSatiety);
-			baron.getAgent().RunSelf(0);
-			allAgents.add(baron);
-			
-			// Collectors
-			for (int j=0; j<numCollectors; ++j) {
-				CollectorAgent collector = new CollectorAgent(kernel, "Collector_" + j, "SOAR_Codes/PRESET_collector_agent.soar", baron, food, foodSatiety, wood);
-				Identifier fieldsRoot = collector.inputLink.CreateIdWME("fields");
-				for (int k=0; k<numFields; ++k) {
-					Field field = new Field(collector, fieldsRoot, "Field_" + k, FieldState.DRY, fieldYield);
-					collector.addField(field);
-				}
-				collector.getAgent().RunSelf(0);
-				baron.addCollector(collector);
-				allAgents.add(collector);
-			}
-			
-			// Builders
-			for (int j=0; j<numBuilders; ++j) {
-				BuilderAgent builder = new BuilderAgent(kernel, "Builder_" + j, "SOAR_Codes/PRESET_builder_agent.soar", baron, food, foodSatiety);
-				builder.getAgent().RunSelf(0);
-				baron.addBuilder(builder);
-				allAgents.add(builder);
-			}
-		}
-		return allAgents;		
-	}
-	
-	public static void main(String[] args)
-	{
-		// Create the kernel
-		final int kernelPort = 27390;
-		Kernel k = Kernel.CreateKernelInNewThread(kernelPort);
-		if (k.HadError())
-		{
-			System.err.println("Error creating kernel: " + k.GetLastErrorDescription());
-			System.exit(1);
-		}
+                if (line.equalsIgnoreCase("x")) {
+                    shutdown();
+                }
 
-		// Create all the agents and load productions
-		int numBarons = 1;
-		int numCollectors = 2;
-		int numBuilders = 0;
-		ArrayList<GeneralAgent> agentsArray = createAgents(k, numBarons, numCollectors, numBuilders);		
-		
-		// Spawn debugger just for testing
-		agentsArray.get(0).getAgent().SpawnDebugger(kernelPort, "libs/soar/SoarJavaDebugger.jar");
+                runAllAgentsOneStep();
+                updateEnvironmentState();
 
-		// Create the Soar environment and add the agents
-		new Environment(agentsArray);
-	}
+                // Necessary delay (ms)
+                delay(1000);
+            }
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        } finally {
+            shutdown();
+        }
+    }
+
+    private void runAllAgentsOneStep() {
+        for (GeneralAgent agent : agents) {
+            agent.runStep();
+        }
+    }
+
+    private void updateEnvironmentState() {
+        for (int i = 0; i < this.agents.size(); ++i) {
+            // Update agents
+            this.agents.get(i).decreaseSatiety();
+        }
+    }
+
+
+    private ArrayList<ExecutorService> initExecutors() {
+        ArrayList<ExecutorService> exec = new ArrayList<ExecutorService>();
+        for (int i = 0; i < this.agents.size(); ++i) {
+            exec.add(Executors.newSingleThreadExecutor());
+        }
+        return exec;
+    }
+
+    private void shutdown() {
+        // Shutdown the Soar interface and the executor service.
+        for (int i = 0; i < this.agents.size(); ++i) {
+            this.agents.get(i).shutdown();
+            this.executors.get(i).shutdown();
+        }
+        System.exit(0);
+    }
+
+    private void delay(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static ArrayList<GeneralAgent> createAgents(Kernel kernel, int numBarons, int numCollectors, int numBuilders) {
+        ArrayList<GeneralAgent> allAgents = new ArrayList<GeneralAgent>();
+
+        int food = 5;
+        int foodSatiety = 15;
+        int wood = 10;
+        int numFields = 2;
+        FieldState fieldState = FieldState.DRY;
+        int fieldYield = 2;
+
+        // Barons
+        for (int i = 0; i < numBarons; ++i) {
+            BaronAgent baron = new BaronAgent(kernel, "Baron_" + i, "SOAR_Codes/PRESET_baron_agent.soar", food, foodSatiety);
+            baron.getAgent().RunSelf(0);
+            allAgents.add(baron);
+
+            // Collectors
+            for (int j = 0; j < numCollectors; ++j) {
+                CollectorAgent collector = new CollectorAgent(kernel, "Collector_" + j, "SOAR_Codes/PRESET_collector_agent.soar", baron, food, foodSatiety, wood);
+                Identifier fieldsRoot = collector.inputLink.CreateIdWME("fields");
+                for (int k = 0; k < numFields; ++k) {
+                    Field field = new Field(collector, fieldsRoot, "Field_" + k, FieldState.DRY, fieldYield);
+                    collector.addField(field);
+                }
+                collector.getAgent().RunSelf(0);
+                baron.addCollector(collector);
+                allAgents.add(collector);
+            }
+
+            // Builders
+            for (int j = 0; j < numBuilders; ++j) {
+                BuilderAgent builder = new BuilderAgent(kernel, "Builder_" + j, "SOAR_Codes/PRESET_builder_agent.soar", baron, food, foodSatiety);
+                builder.getAgent().RunSelf(0);
+                baron.addBuilder(builder);
+                allAgents.add(builder);
+            }
+        }
+        return allAgents;
+    }
+
+    public static void main(String[] args) {
+        // Create the kernel
+        final int kernelPort = 27390;
+        Kernel k = Kernel.CreateKernelInNewThread(kernelPort);
+        if (k.HadError()) {
+            System.err.println("Error creating kernel: " + k.GetLastErrorDescription());
+            System.exit(1);
+        }
+
+        // Create all the agents and load productions
+        int numBarons = 1;
+        int numCollectors = 2;
+        int numBuilders = 0;
+        ArrayList<GeneralAgent> agentsArray = createAgents(k, numBarons, numCollectors, numBuilders);
+
+        // Spawn debugger just for testing
+        agentsArray.get(0).getAgent().SpawnDebugger(kernelPort, "libs/soar/SoarJavaDebugger.jar");
+
+        // Create the Soar environment and add the agents
+        new Environment(agentsArray);
+    }
 }
 
--- src/edu/upc/fib/masd/jav/SoarAgent.java
+++ src/edu/upc/fib/masd/jav/SoarAgent.java
@@ -1,111 +1,97 @@
 package edu.upc.fib.masd.jav;
-import java.util.HashMap;
-import java.util.Map;
+
+import sml.*;
+import sml.Kernel.UpdateEventInterface;
+
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import sml.Agent;
-import sml.Identifier;
-import sml.Kernel;
-import sml.smlUpdateEventId;
-import sml.Kernel.UpdateEventInterface;
-import sml.WMElement;
-import sml.smlRunStepSize;
+public abstract class SoarAgent {
+    protected final Kernel kernel;
 
-public abstract class SoarAgent
-{
-	protected final Kernel kernel;
+    protected final Agent agent;
 
-	protected final Agent agent;
+    // Input-link WME so that we can quickly add additional messages as they come in.
+    protected Identifier inputLink;
 
-	// Input-link WME so that we can quickly add additional messages as they come in.
-	protected Identifier inputLink;
-	
-	// Output-link WME
-	protected Identifier outputLink;
-	
-	// To ask Soar to stop executing.
-	private final AtomicBoolean stopSoar = new AtomicBoolean(true);
+    // Output-link WME
+    protected Identifier outputLink;
+
+    // To ask Soar to stop executing.
+    private final AtomicBoolean stopSoar = new AtomicBoolean(false);
 
 
-	public SoarAgent(Kernel k, String agentName, String productionsFile) {
-		kernel = k;
-		agent = kernel.CreateAgent(agentName);
-		
-		if (agent == null)
-		{
-			System.err.println("Error creating agent: " + kernel.GetLastErrorDescription());
-			System.exit(1);
-		}
+    public SoarAgent(Kernel k, String agentName, String productionsFile) {
+        kernel = k;
+        agent = kernel.CreateAgent(agentName);
 
-		// Load the productions.
-		if (!agent.LoadProductions(productionsFile)) {
-			System.err.println("Can't load " + productionsFile + " file.");
-			System.exit(1);
-		}
+        if (agent == null) {
+            System.err.println("Error creating agent: " + kernel.GetLastErrorDescription());
+            System.exit(1);
+        }
 
-		// Create and cache input-link and output-link
-		inputLink = agent.GetInputLink();
-		outputLink = agent.GetOutputLink();
+        // Load the productions.
+        if (!agent.LoadProductions(productionsFile)) {
+            System.err.println("Can't load " + productionsFile + " file.");
+            System.exit(1);
+        }
 
-		// Event that fires after our agent passes its output phase. 
-		// Post new messages on the input-link and read commands of the output link.
-		kernel.RegisterForUpdateEvent(
-				smlUpdateEventId.smlEVENT_AFTER_ALL_OUTPUT_PHASES,
-				new UpdateEventInterface()
-				{
-					public void updateEventHandler(int eventID,
-							Object data, Kernel kernel, int runFlags)
-					{
-						System.out.println("Agent " + agent.GetAgentName() + " commands received: " + outputLink.GetNumberChildren());
+        // Create and cache input-link and output-link
+        inputLink = agent.GetInputLink();
 
-						// Iterate through the commands on the output link.
-						for (int index = 0; index < outputLink.GetNumberChildren(); ++index)
-						{
-							// Get command
-							WMElement command = outputLink.GetChild(index);
-							treatCommand(command);
-						}
+        // Event that fires after our agent passes its output phase.
+        // Post new messages on the input-link and read commands of the output link.
+        kernel.RegisterForUpdateEvent(smlUpdateEventId.smlEVENT_AFTER_ALL_OUTPUT_PHASES, new UpdateEventInterface() {
+            public void updateEventHandler(int eventID, Object data, Kernel kernel, int runFlags) {
+                outputLink = agent.GetOutputLink();
+                System.out.printf("Agent %s commands received: %d%n", agent.GetAgentName(), outputLink.GetNumberChildren());
 
-						// Mark commands as seen so they will not be encountered again 
-						// if they are still on the output-link then.
-						agent.ClearOutputLinkChanges();
+                // Iterate through the commands on the output link.
+                for (int index = 0; index < outputLink.GetNumberChildren(); ++index) {
+                    // Get command
+                    WMElement command = outputLink.GetChild(index);
+                    treatCommand(command);
+                }
 
-						// Check if we have to stop
-						if (stopSoar.get()) {
-							kernel.StopAllAgents();
-						}
-					}
-				}, null);
-		/*
-		 * That final null parameter above is for user data. Anything passed
-		 * there will appear in the updateEventHandler's Object data
-		 * parameter.
-		 */
-	}
-	
-	public void runStep() {
-		System.out.println("Agent " + agent.GetAgentName() + " run step");
-		this.agent.RunSelf(1, smlRunStepSize.sml_UNTIL_OUTPUT);
-	}
+                // Mark commands as seen so they will not be encountered again
+                // if they are still on the output-link then.
+                agent.ClearOutputLinkChanges();
 
-	public void stop() {
-		// Ask the agent to stop itself during its next update event.
-		stopSoar.set(true);
-	}
+                // Check if we have to stop
+                if (stopSoar.get()) {
+                    kernel.StopAllAgents();
+                }
+            }
+        }, null);
+        /*
+         * That final null parameter above is for user data. Anything passed
+         * there will appear in the updateEventHandler's Object data
+         * parameter.
+         */
+    }
 
-	public void shutdown() {
-		stop();
-		try {
-			Thread.sleep(500);
-		}
-		catch (InterruptedException ignored){}
-		kernel.Shutdown();
-	}
-	
-	public Agent getAgent() {
-		return agent;
-	}
-	
-	public abstract void treatCommand(WMElement command); 
-	
+    public void runStep() {
+        System.out.println("Agent " + agent.GetAgentName() + " run step");
+        this.agent.RunSelf(1, smlRunStepSize.sml_UNTIL_OUTPUT);
+    }
+
+    public void stop() {
+        // Ask the agent to stop itself during its next update event.
+        stopSoar.set(true);
+    }
+
+    public void shutdown() {
+        stop();
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException ignored) {
+        }
+        kernel.Shutdown();
+    }
+
+    public Agent getAgent() {
+        return agent;
+    }
+
+    public abstract void treatCommand(WMElement command);
+
 }
