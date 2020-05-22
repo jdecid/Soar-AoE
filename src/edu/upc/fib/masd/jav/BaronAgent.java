6e2106bbc2c2cc8ed5d57cf2ead483f1212279c9
Josep de Cid <josep.de.cid@gmail.com>
2020-05-23 00:09:07 +0200

    Create inputs for Field and Subordinate

--- src/edu/upc/fib/masd/jav/BaronAgent.java
+++ src/edu/upc/fib/masd/jav/BaronAgent.java
@@ -1,53 +1,54 @@
 package edu.upc.fib.masd.jav;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import sml.Identifier;
 import sml.Kernel;
 import sml.WMElement;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BaronAgent extends GeneralAgent {
-	private Map<String, CollectorAgent> collectors;
-	private Map<String, BuilderAgent> builders;
-	
-	public BaronAgent(Kernel k, String agentName, String productionsFile, int food, int foodSatiety) {
-		super(k, agentName, productionsFile, food, foodSatiety);
-		collectors = new HashMap<String, CollectorAgent>();
-		builders = new HashMap<String, BuilderAgent>();
-	}
-	
-	public void addCollector(CollectorAgent collector) {
-		collectors.put(collector.getAgent().GetAgentName(), collector);
-	}
-	
-	public void addBuilder(BuilderAgent builder) {
-		builders.put(builder.getAgent().GetAgentName(), builder);
-	}
-	
-	public void treatSpecificCommand(WMElement command) {
-		String name = command.GetAttribute();
-		if (name.equals("demand-food")) {
-			
-		}
-		else if (name.equals("demand-wood")) {
-			
-		}
-		else if (name.equals("bestow-food")) {
-			
-		}
-		else if (name.equals("bestow-wood")) {
-			
-		}
-		else if (name.equals("demand-change-profession")) {
-			
-		}
-		else if (name.equals("demand-build-house")) {
-			
-		}
-		else {
-			System.out.println("Command " + name + " not implemented");
-		}
-	}
+    private Map<String, CollectorAgent> collectors;
+    private Map<String, BuilderAgent> builders;
+    private Identifier subordinatesWME;
+
+    public BaronAgent(Kernel k, String agentName, String productionsFile, int food, int foodSatiety) {
+        super(k, agentName, productionsFile, food, foodSatiety);
+        collectors = new HashMap<String, CollectorAgent>();
+        builders = new HashMap<String, BuilderAgent>();
+
+        subordinatesWME = this.inputLink.CreateIdWME("subordinates");
+    }
+
+    public void addCollector(CollectorAgent collector) {
+        collectors.put(collector.getAgent().GetAgentName(), collector);
+
+        Identifier subordinate = subordinatesWME.CreateIdWME("subordinate");
+        subordinate.CreateStringWME("id", collector.getAgent().GetAgentName());
+        subordinate.CreateStringWME("type", "collector");
+    }
+
+    public void addBuilder(BuilderAgent builder) {
+        builders.put(builder.getAgent().GetAgentName(), builder);
+    }
+
+    public void treatSpecificCommand(WMElement command) {
+        String name = command.GetAttribute();
+        if (name.equals("demand-food")) {
+
+        } else if (name.equals("demand-wood")) {
+
+        } else if (name.equals("bestow-food")) {
+
+        } else if (name.equals("bestow-wood")) {
+
+        } else if (name.equals("demand-change-profession")) {
+
+        } else if (name.equals("demand-build-house")) {
+
+        } else {
+            System.out.println("Command " + name + " not implemented");
+        }
+    }
 
 }
--- src/edu/upc/fib/masd/jav/utils/Field.java
+++ src/edu/upc/fib/masd/jav/utils/Field.java
@@ -6,44 +6,46 @@ import sml.IntElement;
 import sml.StringElement;
 
 public class Field {
-	private CollectorAgent agent;
-	private Identifier fieldRootWME;
-	private String id;
-	private StringElement idWME;
-	private FieldState state;
-	private StringElement stateWME;
-	private int yield;
-	private IntElement yieldWME;
-	private int sownRounds;
-	
-	
-	public Field(CollectorAgent agent, Identifier fieldRootWME, String id, FieldState state, int yield) {
-		super();
-		this.agent = agent;
-		this.fieldRootWME = fieldRootWME;
-		this.id = id;
-		idWME = fieldRootWME.CreateStringWME("id", id);
-		this.state = state;
-		stateWME = fieldRootWME.CreateStringWME("state", state.string);
-		this.yield = yield;
-		yieldWME = fieldRootWME.CreateIntWME("yield", yield);
-		this.sownRounds = 0;
-	}
+    private CollectorAgent agent;
+    private Identifier fieldRootWME;
+    private String id;
+    private StringElement idWME;
+    private FieldState state;
+    private StringElement stateWME;
+    private int yield;
+    private IntElement yieldWME;
+    private int sownRounds;
 
-	public String getId() {
-		return id;
-	}
-	
-	public void step() {
-		if (state == FieldState.SOWN) {
-			if (sownRounds < 5) {
-				sownRounds += 1;
-			}
-			else {
-				state = FieldState.HARVESTABLE;
-				sownRounds = 0;
-				agent.getAgent().Update(stateWME, state.string);
-			}
-		}
-	}
+
+    public Field(CollectorAgent agent, Identifier fieldRootWME, String id, FieldState state, int yield) {
+        super();
+        this.agent = agent;
+        this.fieldRootWME = fieldRootWME;
+        Identifier field = this.fieldRootWME.CreateIdWME("field");
+
+        this.id = id;
+
+        idWME = field.CreateStringWME("id", id);
+        this.state = state;
+        stateWME = field.CreateStringWME("state", state.string);
+        this.yield = yield;
+        yieldWME = field.CreateIntWME("yield", yield);
+        this.sownRounds = 0;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void step() {
+        if (state == FieldState.SOWN) {
+            if (sownRounds < 5) {
+                sownRounds += 1;
+            } else {
+                state = FieldState.HARVESTABLE;
+                sownRounds = 0;
+                agent.getAgent().Update(stateWME, state.string);
+            }
+        }
+    }
 }
